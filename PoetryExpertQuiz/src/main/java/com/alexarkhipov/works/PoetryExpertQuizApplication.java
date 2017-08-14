f2cd9b6e6bcea2e75f22f216f6fba93d969697e8
alex-arkhipov <simeon.arkhipov@gmail.com>
2017-08-14 22:28:48 +0300

    First working version with H2 database and sample query for list of authors

--- PoetryExpertQuiz/src/main/java/com/alexarkhipov/works/PoetryExpertQuizApplication.java
+++ PoetryExpertQuiz/src/main/java/com/alexarkhipov/works/PoetryExpertQuizApplication.java
@@ -1,20 +1,10 @@
 package com.alexarkhipov.works;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PoetryExpertQuizApplication {
-
-	@Bean
-	ServletRegistrationBean h2servletRegistration() {
-		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-		registrationBean.addUrlMappings("/console/*");
-		return registrationBean;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PoetryExpertQuizApplication.class, args);
